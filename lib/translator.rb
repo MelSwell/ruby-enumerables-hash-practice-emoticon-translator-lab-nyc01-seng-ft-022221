bf8c4cba2aafafea7d7177eaeb9461d638766f3c
Seth Cohen <smcohen82@gmail.com>
2021-02-09 19:39:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,6 @@ def get_japanese_emoticon(file_path, emoticon)
   target_emoticon = library.keys.find do |key|
     library[key][:english] == emoticon
   end
-  binding.pry
   target_emoticon ? library[target_emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
