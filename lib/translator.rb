267cb66f93e8686f15c1c737e475c6c68427030e
Seth Cohen <smcohen82@gmail.com>
2021-02-09 18:36:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,13 +5,13 @@ require 'pry'
 def load_library(library)
   emoticons = YAML.load_file(library)
   emoticons_hash = {}
-  
+  binding.pry
   emoticons.each do |key, value|
     if !emoticons_hash[key]
       emoticons_hash[key] = value
     end
   end
-
+  
 end
 
 def get_japanese_emoticon
