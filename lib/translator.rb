8c2a4437a8295dc7331f3738edfdeb361a6e753b
Seth Cohen <smcohen82@gmail.com>
2021-02-09 19:13:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,9 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key|
-  binding.pry
+  target_emoticon = emoticons.key.find do |key|
+    emoticons[key][:english] == emoticon
+    binding.pry
   end
   
 end
