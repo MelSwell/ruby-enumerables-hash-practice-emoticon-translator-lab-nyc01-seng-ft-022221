37ad68e899530e85b7a5fb25c95e21cfa4f9b9a4
Seth Cohen <smcohen82@gmail.com>
2021-02-09 18:47:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   
   emoticons.each do |key, value|
-    binding.pry
     emoticons[key][:english] = value.first
+    binding.pry
   end
   emoticons
 end
